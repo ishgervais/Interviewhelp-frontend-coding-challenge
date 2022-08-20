7e46cb945a87754832f3d8da805b9b46cd097610
ishgervais <ish.gervais@gmail.com>
2022-08-21 01:35:58 +0200

    chore:removed the pagination related components

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,13 @@ import Button from './components/Button'
 import Loader from './components/Loader';
 import UserCard from './components/UserCard';
 import { FormatedUserRecord, UserLog, UserRecord, UserRecords } from './types';
-import { config } from "react-spring";
-import VerticalCarousel from "./components/VerticalCarousel.jsx";
 
 function App() {
 
   const [users, setUsers] = useState<UserRecord[]>();
   const [formatedUsers, setFormatedUsers] = useState<FormatedUserRecord[]>([]);
   const [userLogs, setUserLogs] = useState<UserLog[]>([]);
-  const [slides, setSlides] = useState<{ key: number, content: string, offset?: string }[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [sliderOptions, setSliderOptions] = useState({
-    goToSlide: 0,
-    offsetRadius: 2,
-    showNavigation: true,
-    config: config.gentle
-  });
 
   const TABLE_BASE_API_URL = 'https://api.airtable.com/v0/appBTaX8XIvvr6zEC/Users';
   const API_KEY = 'key4v56MUqVr9sNJv';
@@ -43,12 +34,6 @@ function App() {
     });
     const res: UserRecords = await data.json();
     setUsers(res.records);
-    // let index = slides.length + 1;
-    // const newSlides = [...slides, { key: index, content: index.toString(), offset: res.offset }];
-    // if (res.offset) {
-    //   newSlides.push({ key: index + 1, content: (index + 1).toString() });
-    // }
-    // setSlides(newSlides);
   }
 
   const formatUsers = async () => {
@@ -140,12 +125,6 @@ function App() {
       </div>
       <div className="users-container flex">
         <div className="pagination-indicator-container">
-          {/* <VerticalCarousel
-            slides={slides}
-            offsetRadius={sliderOptions.offsetRadius}
-            showNavigation={sliderOptions.showNavigation}
-            animationConfig={sliderOptions.config}
-          /> */}
         </div>
         {
           isLoading ?
